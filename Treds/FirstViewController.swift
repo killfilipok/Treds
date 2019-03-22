69406720ab9b32123a3e51077e2d5394cef23ba5
Philip <killfilipok@gmail.com>
2019-03-22 16:48:16 +0200

    init

---
+++ Treds/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treds
+//
+//  Created by Philip on 3/22/19.
+//  Copyright © 2019 Philip. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Treds/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treds
+//
+//  Created by Philip on 3/22/19.
+//  Copyright © 2019 Philip. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
