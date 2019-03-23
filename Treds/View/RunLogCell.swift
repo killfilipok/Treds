01a413af11b2550bcc4f5ba3baee599400b81d48
Philip <killfilipok@gmail.com>
2019-03-23 16:34:50 +0200

    lesson2

---
+++ Treds/View/RunLogCell.swift
@@ -0,0 +1,24 @@
+//
+//  RunLogCell.swift
+//  Treds
+//
+//  Created by Philip on 3/23/19.
+//  Copyright © 2019 Philip. All rights reserved.
+//
+
+import UIKit
+
+class RunLogCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
