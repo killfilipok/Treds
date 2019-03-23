67d338540f370a8501a4cf0c31641052595a2278
Philip <killfilipok@gmail.com>
2019-03-23 16:00:59 +0200

    finish

--- Treds/Controller/BeginRunVC.swift
+++ Treds/Controller/BeginRunVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  BeginRunVC.swift
 //  Treds
 //
 //  Created by Philip on 3/22/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class BeginRunVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
